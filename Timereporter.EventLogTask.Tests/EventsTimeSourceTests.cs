4762092d0d60e9dca94882ea504cce2883127d5d
jmrnilsson <martin.jr.nilsson@gmail.com>
2019-04-28 20:18:23 +0200

    fixe the ONE test

--- Timereporter.EventLogTask.Tests/EventsTimeSourceTests.cs
+++ Timereporter.EventLogTask.Tests/EventsTimeSourceTests.cs
@@ -1,5 +1,4 @@
-﻿using Moq;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Timereporter.Core;
@@ -9,10 +8,14 @@ using Xunit;
 
 namespace Timereporter.EventLogTask.Tests
 {
-	// Microsoft.NET.Test.Sdk
+	// TODO: Remove Microsoft.NET.Test.Sdk
 	public class EventLogTrackerTests
 	{
-		EventLogTracker tracker = new EventLogTracker(MakeDateTimeValueFactoryMock(), MakeEventLogFactory());
+		private EventLogTracker tracker = new EventLogTracker(MakeDateTimeValueFactoryMock(), MakeEventLogFactory());
+		private const int startYear = 2011;
+		private const int startMonth = 11;
+		private const int startDay = 11;
+		private readonly DateTime start = new DateTime(startYear, startMonth, startDay);
 
 		[Fact]
 		public void Results_Matches_Specified_Range_Exactly()
@@ -20,15 +23,17 @@ namespace Timereporter.EventLogTask.Tests
 			string[] actual = tracker.FindBy(new EventLogQuery("^ESENT$", "Application", new Date(2011, 11, 10)));
 
 			Assert.Contains("2011-11-11", actual[0]);
-			Assert.Contains("2011-11-21", actual.Last());
+			Assert.Contains("2011-11-18", actual.Last());
 
 			bool foundMidvalue = false;
 			foreach(var a in actual)
 			{
-				Assert.DoesNotContain("2011-11-10", a);
-				Assert.DoesNotContain("2011-11-22", a);
+				Assert.DoesNotContain("2011-11-10", a);  // Out of bounds, too early
+				Assert.DoesNotContain("2011-11-22", a);  // Out of bounds, too late
+				Assert.DoesNotContain("2011-11-12", a);  // Saturday
+				Assert.DoesNotContain("2011-11-13", a);  // Sunday
 
-				if (a.Contains("2011-11-12"))
+				if (a.Contains("2011-11-14"))
 				{
 					foundMidvalue = true;
 				}
@@ -41,12 +46,11 @@ namespace Timereporter.EventLogTask.Tests
 			IEnumerable<IEventLogEntryProxy> MakeEventLogEntries()
 			{
 				const int h = 4;
-				DateTime start = new DateTime(2011, 11, 11);
-				DateTime end = new DateTime(2011, 11, 21);
+				DateTime start = new DateTime(startYear, startMonth, startDay);
 				int i = 0;
-				while(start.AddHours(i*h) < end)
+				while(start.AddHours(i*h) < start.AddDays(10))
 				{
-					yield return new EventLogEntryStub(start.AddDays(i), i % 2 == 0 ? "ESENT" : "SOMETHING ELSE");
+					yield return new EventLogEntryStub(start.AddHours(i * h), i % 2 == 0 ? "ESENT" : "SOMETHING ELSE");
 					i++;
 				}
 			}
@@ -56,10 +60,13 @@ namespace Timereporter.EventLogTask.Tests
 
 		private static IDateTimeValueFactory MakeDateTimeValueFactoryMock()
 		{
-			DateTime start = new DateTime(2011, 11, 11);
-			Mock<IDateTimeValueFactory> mock = new Mock<IDateTimeValueFactory>();
-			mock.Setup(m => m.LocalToday(It.IsAny<int>())).Returns(new Date(start.AddDays(-22)));
-			return mock.Object;
+			return new DateTimeValueFactoryFake(startYear, startMonth, startDay);
+
+			// Moq doesn't persist Nuget Install. Wait with this.
+
+			//Mock<IDateTimeValueFactory> mock = new Mock<IDateTimeValueFactory>();
+			//mock.Setup(m => m.LocalToday(It.IsAny<int>())).Returns(new Date(start.AddDays(-22)));
+			//return mock.Object;
 		}
 	}
 }
